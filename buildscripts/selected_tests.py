e9f8bc305dc0b18349b6eeee3d2c7b04ac0c04d8
Lydia Stepanek <lydia.stepanek@mongodb.com>
2020-01-22 12:59:01 -0500

    black for double quotes

--- buildscripts/selected_tests.py
+++ buildscripts/selected_tests.py
@@ -21,13 +21,25 @@ if __name__ == "__main__" and __package__ is None:
 
 import buildscripts.resmokelib.parser  # pylint: disable=wrong-import-position
 import buildscripts.util.read_config as read_config  # pylint: disable=wrong-import-position
-from buildscripts.burn_in_tests import create_task_list_for_tests  # pylint: disable=wrong-import-position
-from buildscripts.ciconfig.evergreen import EvergreenProjectConfig, ResmokeArgs, \
-        parse_evergreen_file  # pylint: disable=wrong-import-position
-from buildscripts.evergreen_generate_resmoke_tasks import CONFIG_FORMAT_FN, \
-        DEFAULT_CONFIG_VALUES, REQUIRED_CONFIG_KEYS, GenerateSubSuites, \
-        SelectedTestsConfigOptions, write_file_dict  # pylint: disable=wrong-import-position
-from buildscripts.patch_builds.change_data import find_changed_files  # pylint: disable=wrong-import-position
+from buildscripts.burn_in_tests import (
+    create_task_list_for_tests,
+)  # pylint: disable=wrong-import-position
+from buildscripts.ciconfig.evergreen import (
+    EvergreenProjectConfig,
+    ResmokeArgs,
+    parse_evergreen_file,
+)  # pylint: disable=wrong-import-position
+from buildscripts.evergreen_generate_resmoke_tasks import (
+    CONFIG_FORMAT_FN,
+    DEFAULT_CONFIG_VALUES,
+    REQUIRED_CONFIG_KEYS,
+    GenerateSubSuites,
+    SelectedTestsConfigOptions,
+    write_file_dict,
+)  # pylint: disable=wrong-import-position
+from buildscripts.patch_builds.change_data import (
+    find_changed_files,
+)  # pylint: disable=wrong-import-position
 
 structlog.configure(logger_factory=LoggerFactory())
 LOGGER = structlog.getLogger(__name__)
@@ -73,15 +85,15 @@ def _get_evg_api(evg_api_config: str, local_mode: bool) -> Optional[EvergreenApi
 
 
 def _check_file_exists_in_repo(repo, file_path: str) -> bool:
-    '''
+    """
     Check if file exists in a repo.
 
     :param repo: The git python repo object.
     :param file_path: The relative path to the file from the repository root.
     return: True if file is found in the repo at the specified path, false otherwise.
-    '''
+    """
     pathdir = os.path.dirname(file_path)
-    #Build up reference to desired repo path
+    # Build up reference to desired repo path
     rsub = repo.head.commit.tree
 
     for path_element in pathdir.split(os.path.sep):
@@ -95,22 +107,25 @@ def _check_file_exists_in_repo(repo, file_path: str) -> bool:
 
 
 def _filter_deleted_files(repo: Repo, related_test_files: Set[str]) -> Set[str]:
-    '''
+    """
     Filter out files from a Set if they do not exist in the given repo.
 
     :param repo: The git python repo object.
     :param related_test_files: Set of files to filer.
     return: Set of files that do exist in repo.
-    '''
+    """
     return {
-        filepath
-        for filepath in related_test_files if _check_file_exists_in_repo(repo, filepath)
+        filepath for filepath in related_test_files if _check_file_exists_in_repo(repo, filepath)
     }
 
 
-def _find_related_test_files(selected_tests_auth_user: str, selected_tests_auth_token: str,
-                             changed_files: Set[str], repo: Repo) -> Set[str]:
-    '''
+def _find_related_test_files(
+    selected_tests_auth_user: str,
+    selected_tests_auth_token: str,
+    changed_files: Set[str],
+    repo: Repo,
+) -> Set[str]:
+    """
     For a list of changed files, make a request to the selected-tests service to return all test
     files related to these changed files, then filter the test files.
 
@@ -119,23 +134,28 @@ def _find_related_test_files(selected_tests_auth_user: str, selected_tests_auth_
     :param changed_files: Set of changed_files.
     :param repo: The git python repo object.
     return: Set of test files returned by selected-tests service that do exist in repo.
-    '''
-    payload = {'threshold': .1, 'changed_files': ",".join(changed_files)}
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    """
+    payload = {"threshold": 0.1, "changed_files": ",".join(changed_files)}
+    headers = {"Content-type": "application/json", "Accept": "application/json"}
     cookies = dict(auth_user=selected_tests_auth_user, auth_token=selected_tests_auth_token)
     response = requests.get(
         "https://selected-tests.server-tig.prod.corp.mongodb.com/projects/mongodb-mongo-master/test-mappings",
-        params=payload, headers=headers, cookies=cookies).json()
+        params=payload,
+        headers=headers,
+        cookies=cookies,
+    ).json()
     related_test_files = {
-        test_file['name']
-        for test_mapping in response['test_mappings'] for test_file in test_mapping['test_files']
+        test_file["name"]
+        for test_mapping in response["test_mappings"]
+        for test_file in test_mapping["test_files"]
     }
     return _filter_deleted_files(repo, related_test_files)
 
 
-def _create_overwrite_values(evg_conf: EvergreenProjectConfig, build_variant: str, task_name: str,
-                             burn_in_task_config: dict):
-    '''
+def _create_overwrite_values(
+    evg_conf: EvergreenProjectConfig, build_variant: str, task_name: str, burn_in_task_config: dict,
+):
+    """
     Create overwrite values to pass in to initialize a SelectedTestsConfigOptions object.
 
     :param evg_conf: Evergreen configuration.
@@ -143,27 +163,27 @@ def _create_overwrite_values(evg_conf: EvergreenProjectConfig, build_variant: st
     :param task_name: Name of task to get info for.
     :param burn_in_task_config: The value for a given task_name in the tests_by_task dict.
     return: Overwrite values.
-    '''
+    """
     evg_build_variant = evg_conf.get_variant(build_variant)
     task = evg_build_variant.get_task(task_name)
     if task.is_generate_resmoke_task:
         task_vars = task.generate_resmoke_tasks_command["vars"]
     else:
         task_vars = task.run_tests_command["vars"]
-        task_vars.update({'fallback_num_sub_suites': '1'})
+        task_vars.update({"fallback_num_sub_suites": "1"})
 
-    tests_to_run = " ".join(burn_in_task_config['tests'])
-    task_vars['resmoke_args'] = "{} {}".format(task_vars['resmoke_args'], tests_to_run)
+    tests_to_run = " ".join(burn_in_task_config["tests"])
+    task_vars["resmoke_args"] = "{} {}".format(task_vars["resmoke_args"], tests_to_run)
 
     overwrite_values = {"task_name": task_name, **task_vars}
-    suite_name = ResmokeArgs.get_arg(task_vars['resmoke_args'], "suites")
+    suite_name = ResmokeArgs.get_arg(task_vars["resmoke_args"], "suites")
     if suite_name:
         overwrite_values.update({"suite": suite_name})
     return overwrite_values
 
 
 def _generate_shrub_config(evg_api, evg_conf, expansion_file, tests_by_task, build_variant):
-    '''
+    """
     Generate a dict containing file names and contents for the json file containing generated
     tasks config and the yml files containing suite configs.
 
@@ -173,18 +193,24 @@ def _generate_shrub_config(evg_api, evg_conf, expansion_file, tests_by_task, bui
     :param tests_by_task: Dictionary of tests and tasks to run.
     :param build_variant: Build variant to collect task info from.
     return: Dict of files and file contents for generated tasks.
-    '''
+    """
     shrub_config = Configuration()
     config_file_dict = {}
     shrub_config_json = {}
     for task_name, burn_in_task_config in tests_by_task.items():
-        overwrite_values = _create_overwrite_values(evg_conf, build_variant, task_name,
-                                                    burn_in_task_config)
+        overwrite_values = _create_overwrite_values(
+            evg_conf, build_variant, task_name, burn_in_task_config
+        )
         config_options = SelectedTestsConfigOptions.from_file(
-            expansion_file, overwrite_values, REQUIRED_CONFIG_KEYS, DEFAULT_CONFIG_VALUES,
-            CONFIG_FORMAT_FN)
+            expansion_file,
+            overwrite_values,
+            REQUIRED_CONFIG_KEYS,
+            DEFAULT_CONFIG_VALUES,
+            CONFIG_FORMAT_FN,
+        )
         suite_file_dict, shrub_config_json = GenerateSubSuites(
-            evg_api, config_options).generate_config_dict(shrub_config)
+            evg_api, config_options
+        ).generate_config_dict(shrub_config)
         config_file_dict.update(suite_file_dict)
     config_file_dict["selected_tests_config.json"] = shrub_config_json
     return config_file_dict
@@ -192,21 +218,56 @@ def _generate_shrub_config(evg_api, evg_conf, expansion_file, tests_by_task, bui
 
 @click.command()
 @click.option("--verbose", "verbose", default=False, is_flag=True, help="Enable extra logging.")
-@click.option("--expansion-file", "expansion_file", type=str, required=True,
-              help="Location of expansions file generated by evergreen.")
-@click.option("--evg-api-config", "evg_api_config", default=EVG_CONFIG_FILE, metavar="FILE",
-              help="Configuration file with connection info for Evergreen API.")
-@click.option("--local", "local_mode", default=False, is_flag=True,
-              help="Local mode. Do not call out to evergreen api.")
-@click.option("--build-variant", "build_variant", default=None, metavar='BUILD_VARIANT',
-              help="Tasks to run will be selected from this build variant.")
-@click.option("--selected-tests-auth-user", "selected_tests_auth_user", required=True,
-              help="Auth user for selected-tests service.")
-@click.option("--selected-tests-auth-token", "selected_tests_auth_token", required=True,
-              help="Auth token for selected-tests service.")
+@click.option(
+    "--expansion-file",
+    "expansion_file",
+    type=str,
+    required=True,
+    help="Location of expansions file generated by evergreen.",
+)
+@click.option(
+    "--evg-api-config",
+    "evg_api_config",
+    default=EVG_CONFIG_FILE,
+    metavar="FILE",
+    help="Configuration file with connection info for Evergreen API.",
+)
+@click.option(
+    "--local",
+    "local_mode",
+    default=False,
+    is_flag=True,
+    help="Local mode. Do not call out to evergreen api.",
+)
+@click.option(
+    "--build-variant",
+    "build_variant",
+    default=None,
+    metavar="BUILD_VARIANT",
+    help="Tasks to run will be selected from this build variant.",
+)
+@click.option(
+    "--selected-tests-auth-user",
+    "selected_tests_auth_user",
+    required=True,
+    help="Auth user for selected-tests service.",
+)
+@click.option(
+    "--selected-tests-auth-token",
+    "selected_tests_auth_token",
+    required=True,
+    help="Auth token for selected-tests service.",
+)
 # pylint: disable=too-many-arguments
-def main(verbose, expansion_file, evg_api_config, local_mode, build_variant,
-         selected_tests_auth_user, selected_tests_auth_token):
+def main(
+    verbose,
+    expansion_file,
+    evg_api_config,
+    local_mode,
+    build_variant,
+    selected_tests_auth_user,
+    selected_tests_auth_token,
+):
     """Execute Main program."""
     _configure_logging(verbose)
 
@@ -217,14 +278,16 @@ def main(verbose, expansion_file, evg_api_config, local_mode, build_variant,
     changed_files = find_changed_files(repo)
     buildscripts.resmokelib.parser.set_options()
     LOGGER.debug("Found changed files", files=changed_files)
-    related_test_files = _find_related_test_files(selected_tests_auth_user,
-                                                  selected_tests_auth_token, changed_files, repo)
+    related_test_files = _find_related_test_files(
+        selected_tests_auth_user, selected_tests_auth_token, changed_files, repo
+    )
     LOGGER.debug("related test files found", related_test_files=related_test_files)
     if related_test_files:
         tests_by_task = create_task_list_for_tests(related_test_files, build_variant, evg_conf)
         LOGGER.debug("tests and tasks found", tests_by_task=tests_by_task)
-        config_file_dict = _generate_shrub_config(evg_api, evg_conf, expansion_file, tests_by_task,
-                                                  build_variant)
+        config_file_dict = _generate_shrub_config(
+            evg_api, evg_conf, expansion_file, tests_by_task, build_variant
+        )
 
         write_file_dict(SELECTED_TESTS_CONFIG_DIR, config_file_dict)
     else:
--- buildscripts/tests/test_selected_tests.py
+++ buildscripts/tests/test_selected_tests.py
@@ -19,7 +19,7 @@ def ns(relative_name):  # pylint: disable=invalid-name
 
 def repo_with_one_files_1_and_2(temp_directory):
     repo = git.Repo.init(temp_directory)
-    subdir = os.path.join(temp_directory, 'jstests')
+    subdir = os.path.join(temp_directory, "jstests")
     os.makedirs(subdir)
     file_1 = os.path.join(subdir, "file-1.js")
     file_2 = os.path.join(subdir, "file-2.js")
@@ -33,24 +33,37 @@ def repo_with_one_files_1_and_2(temp_directory):
 def tests_by_task_stub():
     return {
         "jsCore_auth": {
-            'display_task_name': 'jsCore_auth', 'resmoke_args': '--suites=core_auth', 'tests': [
-                'jstests/core/currentop_waiting_for_latch.js', 'jstests/core/latch_analyzer.js'
-            ], 'use_multiversion': None, 'distro': 'rhel62-small'
-        }, "auth_gen": {
-            'display_task_name': 'auth', 'resmoke_args': '--suites=auth --storageEngine=wiredTiger',
-            'tests': ['jstests/auth/auth3.js'], 'use_multiversion': None, 'distro': 'rhel62-small'
-        }
+            "display_task_name": "jsCore_auth",
+            "resmoke_args": "--suites=core_auth",
+            "tests": [
+                "jstests/core/currentop_waiting_for_latch.js",
+                "jstests/core/latch_analyzer.js",
+            ],
+            "use_multiversion": None,
+            "distro": "rhel62-small",
+        },
+        "auth_gen": {
+            "display_task_name": "auth",
+            "resmoke_args": "--suites=auth --storageEngine=wiredTiger",
+            "tests": ["jstests/auth/auth3.js"],
+            "use_multiversion": None,
+            "distro": "rhel62-small",
+        },
     }
 
 
 def generate_resmoke_task_stub(task_name):
     task_dict = {
-        "name":
-            task_name,
-        "commands": [{
-            "func": "generate resmoke tasks",
-            "vars": {'fallback_num_sub_suites': '4', 'resmoke_args': '--storageEngine=wiredTiger'}
-        }]
+        "name": task_name,
+        "commands": [
+            {
+                "func": "generate resmoke tasks",
+                "vars": {
+                    "fallback_num_sub_suites": "4",
+                    "resmoke_args": "--storageEngine=wiredTiger",
+                },
+            }
+        ],
     }
     return _evergreen.Task(task_dict)
 
@@ -58,7 +71,7 @@ def generate_resmoke_task_stub(task_name):
 def non_generate_resmoke_task_stub(task_name):
     task_dict = {
         "name": task_name,
-        "commands": [{"func": "run tests", "vars": {"resmoke_args": "--suites=core_auth"}}]
+        "commands": [{"func": "run tests", "vars": {"resmoke_args": "--suites=core_auth"}}],
     }
     return _evergreen.Task(task_dict)
 
@@ -98,22 +111,24 @@ class TestFindRelatedTestFiles(unittest.TestCase):
         changed_files = {"src/file1.cpp", "src/file2.js"}
         response_object = {
             "test_mappings": [
-                {"source_file": "src/file1.cpp", "test_files": [{"name": "jstests/file-1.js"}]},
-                {"source_file": "src/file2.cpp", "test_files": [{"name": "jstests/file-3.js"}]},
+                {"source_file": "src/file1.cpp", "test_files": [{"name": "jstests/file-1.js"}],},
+                {"source_file": "src/file2.cpp", "test_files": [{"name": "jstests/file-3.js"}],},
             ]
         }
         requests_mock.get.return_value.json.return_value = response_object
         with TemporaryDirectory() as tmpdir:
             repo = repo_with_one_files_1_and_2(tmpdir)
 
-            related_test_files = under_test._find_related_test_files("auth_user", "auth_token",
-                                                                     changed_files, repo)
+            related_test_files = under_test._find_related_test_files(
+                "auth_user", "auth_token", changed_files, repo
+            )
 
             requests_mock.get.assert_called_with(
-                'https://selected-tests.server-tig.prod.corp.mongodb.com/projects/mongodb-mongo-master/test-mappings',
-                params={'threshold': 0.1, 'changed_files': ",".join(changed_files)}, headers={
-                    'Content-type': 'application/json', 'Accept': 'application/json'
-                }, cookies={"auth_user": "auth_user", "auth_token": "auth_token"})
+                "https://selected-tests.server-tig.prod.corp.mongodb.com/projects/mongodb-mongo-master/test-mappings",
+                params={"threshold": 0.1, "changed_files": ",".join(changed_files)},
+                headers={"Content-type": "application/json", "Accept": "application/json",},
+                cookies={"auth_user": "auth_user", "auth_token": "auth_token"},
+            )
             self.assertEqual(related_test_files, {"jstests/file-1.js"})
 
     @patch(ns("requests"))
@@ -124,8 +139,9 @@ class TestFindRelatedTestFiles(unittest.TestCase):
         with TemporaryDirectory() as tmpdir:
             repo = repo_with_one_files_1_and_2(tmpdir)
 
-            related_test_files = under_test._find_related_test_files("auth_user", "auth_token",
-                                                                     changed_files, repo)
+            related_test_files = under_test._find_related_test_files(
+                "auth_user", "auth_token", changed_files, repo
+            )
 
             self.assertEqual(related_test_files, set())
 
@@ -138,13 +154,15 @@ class TestGetOverwriteValues(unittest.TestCase):
         evg_conf_mock = MagicMock()
         evg_conf_mock.get_variant.return_value.get_task.return_value = task
 
-        overwrite_values = under_test._create_overwrite_values(evg_conf_mock, "variant", task_name,
-                                                               burn_in_task_config)
+        overwrite_values = under_test._create_overwrite_values(
+            evg_conf_mock, "variant", task_name, burn_in_task_config
+        )
 
         self.assertEqual(overwrite_values["task_name"], task_name)
         self.assertIsNone(overwrite_values.get("suite"))
-        self.assertEqual(overwrite_values["resmoke_args"],
-                         '--storageEngine=wiredTiger jstests/auth/auth3.js')
+        self.assertEqual(
+            overwrite_values["resmoke_args"], "--storageEngine=wiredTiger jstests/auth/auth3.js",
+        )
         self.assertEqual(overwrite_values["fallback_num_sub_suites"], "4")
 
     def test_task_is_not_a_generate_resmoke_task(self):
@@ -154,14 +172,15 @@ class TestGetOverwriteValues(unittest.TestCase):
         evg_conf_mock = MagicMock()
         evg_conf_mock.get_variant.return_value.get_task.return_value = task
 
-        overwrite_values = under_test._create_overwrite_values(evg_conf_mock, "variant", task_name,
-                                                               burn_in_task_config)
+        overwrite_values = under_test._create_overwrite_values(
+            evg_conf_mock, "variant", task_name, burn_in_task_config
+        )
 
         self.assertEqual(overwrite_values["task_name"], task_name)
         self.assertEqual(overwrite_values["suite"], "core_auth")
         self.assertEqual(
             overwrite_values["resmoke_args"],
-            '--suites=core_auth jstests/core/currentop_waiting_for_latch.js jstests/core/latch_analyzer.js'
+            "--suites=core_auth jstests/core/currentop_waiting_for_latch.js jstests/core/latch_analyzer.js",
         )
         self.assertEqual(overwrite_values["fallback_num_sub_suites"], "1")
 
@@ -170,43 +189,57 @@ class TestGenerateShrubConfig(unittest.TestCase):
     @patch(ns("_create_overwrite_values"))
     @patch(ns("SelectedTestsConfigOptions"))
     @patch(ns("GenerateSubSuites"))
-    def test_when_test_by_task_returned(self, generate_subsuites_mock,
-                                        selected_tests_config_options_mock,
-                                        get_overwrite_values_mock):
+    def test_when_test_by_task_returned(
+        self,
+        generate_subsuites_mock,
+        selected_tests_config_options_mock,
+        get_overwrite_values_mock,
+    ):
         evg_api = MagicMock()
         evg_conf = MagicMock()
         expansion_file = MagicMock()
         tests_by_task = tests_by_task_stub()
         yml_suite_file_contents = MagicMock()
         shrub_json_file_contents = MagicMock()
-        suite_file_dict_mock = {'auth_0.yml': yml_suite_file_contents}
+        suite_file_dict_mock = {"auth_0.yml": yml_suite_file_contents}
         generate_subsuites_mock.return_value.generate_config_dict.return_value = (
-            suite_file_dict_mock, shrub_json_file_contents)
+            suite_file_dict_mock,
+            shrub_json_file_contents,
+        )
 
-        config_file_dict = under_test._generate_shrub_config(evg_api, evg_conf, expansion_file,
-                                                             tests_by_task, "variant")
+        config_file_dict = under_test._generate_shrub_config(
+            evg_api, evg_conf, expansion_file, tests_by_task, "variant"
+        )
         self.assertEqual(
-            config_file_dict, {
-                'auth_0.yml': yml_suite_file_contents,
-                'selected_tests_config.json': shrub_json_file_contents
-            })
+            config_file_dict,
+            {
+                "auth_0.yml": yml_suite_file_contents,
+                "selected_tests_config.json": shrub_json_file_contents,
+            },
+        )
 
     @patch(ns("_create_overwrite_values"))
     @patch(ns("SelectedTestsConfigOptions"))
     @patch(ns("GenerateSubSuites"))
-    def test_when_no_test_by_task_returned(self, generate_subsuites_mock,
-                                           selected_tests_config_options_mock,
-                                           get_overwrite_values_mock):
+    def test_when_no_test_by_task_returned(
+        self,
+        generate_subsuites_mock,
+        selected_tests_config_options_mock,
+        get_overwrite_values_mock,
+    ):
         evg_api = MagicMock()
         evg_conf = MagicMock()
         expansion_file = MagicMock()
         tests_by_task = {}
         yml_suite_file_contents = MagicMock()
         shrub_json_file_contents = MagicMock()
-        suite_file_dict_mock = {'auth_0.yml': yml_suite_file_contents}
+        suite_file_dict_mock = {"auth_0.yml": yml_suite_file_contents}
         generate_subsuites_mock.return_value.generate_config_dict.return_value = (
-            suite_file_dict_mock, shrub_json_file_contents)
+            suite_file_dict_mock,
+            shrub_json_file_contents,
+        )
 
-        config_file_dict = under_test._generate_shrub_config(evg_api, evg_conf, expansion_file,
-                                                             tests_by_task, "variant")
-        self.assertEqual(config_file_dict, {'selected_tests_config.json': {}})
+        config_file_dict = under_test._generate_shrub_config(
+            evg_api, evg_conf, expansion_file, tests_by_task, "variant"
+        )
+        self.assertEqual(config_file_dict, {"selected_tests_config.json": {}})
