ee9137a3672a772047842ac6a2dca9f12452eba3
Lydia Stepanek <lydia.stepanek@mongodb.com>
2020-01-31 14:30:39 -0500

    wip done i think

--- buildscripts/selected_tests.py
+++ buildscripts/selected_tests.py
@@ -25,6 +25,7 @@ from buildscripts.burn_in_tests import create_task_list_for_tests, is_file_a_tes
 from buildscripts.ciconfig.evergreen import (
     EvergreenProjectConfig,
     ResmokeArgs,
+    Task,
     parse_evergreen_file,
 )
 from buildscripts.evergreen_generate_resmoke_tasks import (
@@ -162,33 +163,37 @@ def _get_selected_tests_task_configuration(expansion_file):
     }
 
 
-def _get_evg_task_configuration(
-        evg_conf: EvergreenProjectConfig,
-        build_variant: str,
-        task_name: str,
-        test_list_info: dict,
-):
+def _get_task(evg_conf: EvergreenProjectConfig, build_variant: str, task_name: str):
     """
-    Look up task config of the task to be generated.
+    Look up task config for task.
 
     :param evg_conf: Evergreen configuration.
     :param build_variant: Build variant to collect task info from.
     :param task_name: Name of task to get info for.
-    :param test_list_info: The value for a given task_name in the tests_by_task dict.
-    return: Task configuration values.
+    return: Task configuration.
     """
     evg_build_variant = evg_conf.get_variant(build_variant)
     task = evg_build_variant.get_task(task_name)
+    if not task:
+        task = evg_build_variant.get_task(task_name + "_gen")
+    return task
+
+
+def _get_evg_task_configuration(task: Task, build_variant: str):
+    """
+    Look up task config of the task to be generated.
+
+    :param task: Task to get info for.
+    :param build_variant: Build variant to collect task info from.
+    return: Task configuration values.
+    """
     if task.is_generate_resmoke_task:
         task_vars = task.generate_resmoke_tasks_command["vars"]
     else:
         task_vars = task.run_tests_command["vars"]
         task_vars.update({"fallback_num_sub_suites": "1"})
 
-    evg_task_config = {
-        "task_name": task_name, "build_variant": build_variant,
-        "selected_tests_to_run": test_list_info["tests"], **task_vars
-    }
+    evg_task_config = {"task_name": task.name, "build_variant": build_variant, **task_vars}
     suite_name = ResmokeArgs.get_arg(task_vars["resmoke_args"], "suites")
     if suite_name:
         evg_task_config.update({"suite": suite_name})
@@ -196,36 +201,41 @@ def _get_evg_task_configuration(
 
 
 def _generate_shrub_config(evg_api: EvergreenApi, evg_conf: EvergreenProjectConfig,
-                           expansion_file: str, tests_by_task: dict, build_variant: str):
+                           expansion_file: str, related_tasks: List[str], build_variant: str):
     """
     Generate a dict containing file names and contents for the generated configs.
 
     :param evg_api: Evergreen API object.
     :param evg_conf: Evergreen configuration.
     :param expansion_file: Configuration file.
-    :param tests_by_task: Dictionary of tests and tasks to run.
+    :param related_tasks: List of tasks to run.
     :param build_variant: Build variant to collect task info from.
     return: Dict of files and file contents for generated tasks.
     """
     shrub_config = Configuration()
     shrub_task_config = None
     config_dict_of_generated_tasks = {}
-    for task_name, test_list_info in tests_by_task.items():
-        evg_task_config = _get_evg_task_configuration(evg_conf, build_variant, task_name,
-                                                      test_list_info)
-        selected_tests_task_config = _get_selected_tests_task_configuration(expansion_file)
-        evg_task_config.update(selected_tests_task_config)
-        LOGGER.debug("Calculated overwrite_values", overwrite_values=evg_task_config)
-        config_options = SelectedTestsConfigOptions.from_file(
-            expansion_file,
-            evg_task_config,
-            REQUIRED_CONFIG_KEYS,
-            DEFAULT_CONFIG_VALUES,
-            CONFIG_FORMAT_FN,
-        )
-        suite_files_dict, shrub_task_config = GenerateSubSuites(
-            evg_api, config_options).generate_task_config_and_suites(shrub_config)
-        config_dict_of_generated_tasks.update(suite_files_dict)
+    for task_name in related_tasks:
+        task = _get_task(evg_conf, build_variant, task_name)
+        if not task:
+            LOGGER.warning(f"The variant does not contain task", build_variant=build_variant,
+                           task_name=task_name)
+            continue
+        else:
+            evg_task_config = _get_evg_task_configuration(task, build_variant)
+            selected_tests_task_config = _get_selected_tests_task_configuration(expansion_file)
+            evg_task_config.update(selected_tests_task_config)
+            LOGGER.debug("Calculated overwrite_values", overwrite_values=evg_task_config)
+            config_options = SelectedTestsConfigOptions.from_file(
+                expansion_file,
+                evg_task_config,
+                REQUIRED_CONFIG_KEYS,
+                DEFAULT_CONFIG_VALUES,
+                CONFIG_FORMAT_FN,
+            )
+            suite_files_dict, shrub_task_config = GenerateSubSuites(
+                evg_api, config_options).generate_task_config_and_suites(shrub_config)
+            config_dict_of_generated_tasks.update(suite_files_dict)
     if shrub_task_config:
         config_dict_of_generated_tasks["selected_tests_config.json"] = shrub_task_config
     return config_dict_of_generated_tasks
@@ -287,13 +297,20 @@ def main(
     changed_files = find_changed_files(repo)
     buildscripts.resmokelib.parser.set_options()
     LOGGER.debug("Found changed files", files=changed_files)
-    related_test_files = _find_related_test_files(selected_tests_service, changed_files)
-    LOGGER.debug("related test files found", related_test_files=related_test_files)
-    if related_test_files:
-        tests_by_task = create_task_list_for_tests(related_test_files, build_variant, evg_conf)
-        LOGGER.debug("tests and tasks found", tests_by_task=tests_by_task)
+    #  related_test_files = _find_related_test_files(selected_tests_service, changed_files)
+    #  LOGGER.debug("related test files found", related_test_files=related_test_files)
+    #  if related_test_files:
+    #  tests_by_task = create_task_list_for_tests(related_test_files, build_variant, evg_conf)
+    #  LOGGER.debug("tests and tasks found", tests_by_task=tests_by_task)
+    #  config_dict_of_generated_tasks = _generate_shrub_config(evg_api, evg_conf, expansion_file,
+    #  tests_by_task, build_variant)
+
+    #  write_file_dict(SELECTED_TESTS_CONFIG_DIR, config_dict_of_generated_tasks)
+    related_tasks = _find_related_tasks(selected_tests_service, changed_files)
+    LOGGER.debug("related tasks found", related_tasks=related_tasks)
+    if related_tasks:
         config_dict_of_generated_tasks = _generate_shrub_config(evg_api, evg_conf, expansion_file,
-                                                                tests_by_task, build_variant)
+                                                                related_tasks, build_variant)
 
         write_file_dict(SELECTED_TESTS_CONFIG_DIR, config_dict_of_generated_tasks)
 
